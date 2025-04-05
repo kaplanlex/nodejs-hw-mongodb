8b6ac3b4cd1b96b771f9b8baa99b538cbd88ebb4
AlexKaplan <alexkaplan110606@gmail.com>
2025-04-05 15:08:23 +0300

    fixed port

--- src/server.js
+++ src/server.js
@@ -10,39 +10,26 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
-
   app.use(express.json());
+
   app.use(cors());
+
 
   app.use(
     pino({
       transport: {
         target: 'pino-pretty',
       },
-    })
+    }),
   );
 
-  app.use('/api/contacts', contactsRouter);
+  app.use(contactsRouter);
 
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
 
-  return app;
-};
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  const app = setupServer();
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason);
-});
+};
