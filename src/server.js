5ac93b701593d76fd570cefaf3dd5e897c5360bb
AlexKaplan <alexkaplan110606@gmail.com>
2025-04-05 14:54:21 +0300

    add hw3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import { getAllContacts, getContactById } from './services/contacts.js';
-
+import contactsRouter from './routers/contacts.js';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler.js';
 import express from 'express';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import pino from 'pino-http';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -11,7 +12,6 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(cors());
 
   app.use(
@@ -19,56 +19,30 @@ export const setupServer = () => {
       transport: {
         target: 'pino-pretty',
       },
-    }),
+    })
   );
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
+  app.use('/api/contacts', contactsRouter);
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use('*', notFoundHandler);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(errorHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+  return app;
+};
 
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+if (import.meta.url === `file://${process.argv[1]}`) {
+  const app = setupServer();
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-};
+}
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+});
