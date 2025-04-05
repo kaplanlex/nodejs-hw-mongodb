82b31d7782d18dbf30408bb0b1045d8a3d8bc3f7
AlexKaplan <alexkaplan110606@gmail.com>
2025-04-05 15:23:54 +0300

    fixed error 505

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (Object.getPrototypeOf(err) === HttpError.prototype) {
+  if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
       message: err.name,
@@ -10,7 +10,7 @@ export const errorHandler = (err, req, res, next) => {
     return;
   }
 
-  res.status(500).json({
+  res.json({
     status: 500,
     message: 'Something went wrong',
     data: err.message,
