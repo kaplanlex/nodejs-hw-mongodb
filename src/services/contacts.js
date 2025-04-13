0269a493cbc87fbff532eb58f22d0259f3f468fe
AlexKaplan <alexkaplan110606@gmail.com>
2025-04-13 19:39:59 +0300

    fixed params

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,6 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 import { SORT_ORDER } from '../constants/index.js';
-import { paginationData } from '../utils/paginationData.js';
+import { calculatePaginationData } from '../utils/paginationData.js';
 
 export const getAllContacts = async ({
   page = 1,
@@ -8,19 +8,16 @@ export const getAllContacts = async ({
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
   filter = {},
-}) => {
+} = {}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
-
   const contactsQuery = ContactsCollection.find();
-
   if (filter.type) {
     contactsQuery.where('contactType').equals(filter.type);
   }
   if (typeof filter.isFavorite === 'boolean') {
     contactsQuery.where('isFavorite').equals(filter.isFavorite);
   }
-
   const [contactsCount, contacts] = await Promise.all([
     ContactsCollection.find().merge(contactsQuery).countDocuments(),
     contactsQuery
@@ -30,18 +27,13 @@ export const getAllContacts = async ({
       .exec(),
   ]);
 
-  const paginationContactData = PaginationData(contactsCount, perPage, page);
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
   return {
     data: contacts,
-    ...paginationContactData,
+    ...paginationData,
   };
 };
-//---------------
-// export const getAllContacts = async () => {
-//   const contacts = await ContactsCollection.find();
-//   return contacts;
-// };
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
--- src/utils/paginationData.js
+++ src/utils/paginationData.js
@@ -1,4 +1,4 @@
-export const paginationData = (count, perPage, page) => {
+export const calculatePaginationData = (count, perPage, page) => {
   const totalPages = Math.ceil(count / perPage);
   const hasNextPage = Boolean(totalPages - page);
   const hasPreviousPage = page !== 1;
